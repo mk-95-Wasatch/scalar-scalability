5ebfb62eab2e2c475b517a9ab2e8fe872175a850
mokbel-karam <mokbel.karam@chemeng.utah.edu>
2022-08-25 11:47:46 -0600

    Added results for scalar 3, 5, and 10

---
+++ scalars-3/extractData.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+here=$PWD
+cases="$(find ${here} -name "outputs")"
+
+# solverDataPaths="solverData"
+scalingDataPaths="scalingData"
+scalingMPIStatsPaths="mpiTimes"
+aveComponentTimesPaths="aveComponentTimes"
+
+for case in $cases;
+do 
+    echo "working on ${case}"
+    cd $case
+    # run the extraction scripts
+    # ./extractSolverScalingData *.out
+    ./extractScalingData *.out
+    ./extractMPIStats *.out
+
+    # solverDataPaths+=("${case}/solverScalingData")
+    scalingDataPaths+=("${case}/scalingData")
+    scalingMPIStatsPaths+=("${case}/mpiTimes")
+    aveComponentTimesPaths+=("${case}/aveComponentTimes")
+done
+cd $here
+
+# echo "${solverDataPaths[*]}" > $here/dataPaths 
+echo "${scalingDataPaths[*]}"> $here/dataPaths 
+echo "${scalingMPIStatsPaths[*]}">> $here/dataPaths
+echo "${aveComponentTimesPaths[*]}">> $here/dataPaths
+
+python $here/dataFrameGen.py
